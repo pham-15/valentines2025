d3cc45fc28fc65a646ddbdc4a06bb1fbf967beaf
Alex Pham <136045296+pham-15@users.noreply.github.com>
2025-02-09 16:51:28 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("yes").addEventListener("click", function () {
+  document.getElementById("question").textContent = "Yay! ❤️";
+});
+
+document.getElementById("no").addEventListener("mouseover", function () {
+  let button = this;
+  button.style.position = "absolute";
+  button.style.left = Math.random() * window.innerWidth + "px";
+  button.style.top = Math.random() * window.innerHeight + "px";
+});
